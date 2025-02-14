f63294afec007c98f4c4ccd7330f2aadcae1c326
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-14 11:33:28 +0300

    add pipeline

---
+++ ui_tests.groovy
@@ -0,0 +1,44 @@
+
+timeout(time: 30, unit: 'MINUTES') {
+
+    node('gradle') {
+
+        utils = load './utils.groovy'
+        utils.prepare_yaml_config()
+
+        stage('Checkout') {
+            checkout scm
+        }
+
+        stage('Checkout utils') {
+            dir('utils') {
+                git branch: 'master', url: 'https://github.com/cristina-dautova/otus-test-runner.git', credentialId: 'jenkins'
+            }
+        }
+
+        utils = load './tools/utils'
+        utils.prepare_yaml_config
+
+        stage('Running UI tests') {
+
+            status = sh(
+                    script: "gradle test",
+                    returnStatus: true
+            )
+
+            if (status > 0) {
+                currentBuild.status = 'UNSTABLE'
+            }
+        }
+
+//        stage('Publish allure report') {
+//            allure ([
+//                    disabled: true,
+//                    includeProperties: false,
+//                    jdk: '',
+//                    report: './target/allure-results',
+//                    reportBuildPolicy: 'ALWAYS'
+//            ])
+//        }
+    }
+}
